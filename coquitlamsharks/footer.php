e34fef1d8ac0902b85e0e7b6831287619f8ec2df
Dmitry Khrisanov <dmitry@tradablebits.com>
2015-04-13 14:09:01 -0700

    initial commit

---
+++ coquitlamsharks/footer.php
@@ -0,0 +1,18 @@
+    <div class="sharks-footer">
+        <?php
+            global $dm_settings;
+            if ($dm_settings['author_credits'] != 0) : ?>
+                <div class="row sharks-author-credits">
+                    <p class="text-center"><a href="<?php global $developer_uri; echo esc_url($developer_uri); ?>">DevDmBootstrap3 <?php _e('created by','devdmbootstrap3') ?> Danny Machal</a></p>
+                </div>
+        <?php endif; ?>
+
+        <?php get_template_part('template-part', 'footernav'); ?>
+    </div>
+
+</div>
+<!-- end main container -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
