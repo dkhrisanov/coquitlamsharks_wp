50f265815f5fe0781083da893fef5c278c0b6b27
Dmitry Khrisanov <dmitry@tradablebits.com>
2015-04-13 21:06:33 -0700

    initial commit

--- coquitlamsharks/template-part-footernav.php
+++ coquitlamsharks/template-part-footernav.php
@@ -1,20 +1,25 @@
 <?php if ( has_nav_menu( 'footer_menu' ) ) : ?>
-<div class="row">
-    <div class="col-sm-8">
-        <?php
-        wp_nav_menu( array(
-                'theme_location'    => 'footer_menu',
-                'depth'             => 2,
-                'container'         => 'div',
-                'container_class'   => 'sharks-footer-link',
-                'menu_class'        => 'link',
-                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                'walker'            => new wp_bootstrap_navwalker())
-        );
-        ?>
-    </div>
-    <div class="col-sm-4 hidden-xs center;">
-        <img src="<?php echo get_template_directory_uri() . '/img/logo_bcgov-150x58.gif' ?>" />
+<div class="container">
+    <div class="row">
+        <div class="col-sm-8">
+            <?php
+            wp_nav_menu( array(
+                    'theme_location'    => 'footer_menu',
+                    'depth'             => 2,
+                    'container'         => 'div',
+                    'container_class'   => 'sharks-footer',
+                    'menu_class'        => 'sharks-footer-link',
+                    'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                    'walker'            => new wp_bootstrap_navwalker())
+            );
+            ?>
+        </div>
+        <div class="col-sm-4 hidden-xs center;">
+            <div style="margin: 40px auto;padding:20px;width:200px;height:100px;">
+                we acknowledge the financial assistance of the Province of British Columbia
+                <img src="<?php echo get_template_directory_uri() . '/img/best-place-on-earth-h-colour.jpg' ?>" />
+            </div>
+        </div>
     </div>
 </div>
 <?php endif; ?>
