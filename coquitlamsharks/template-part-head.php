20018ae168b763b373bc4d8901c0795dbe28140e
Dmitry Khrisanov <dmitry@tradablebits.com>
2015-04-21 20:24:09 -0700

    front page addition

--- coquitlamsharks/template-part-head.php
+++ coquitlamsharks/template-part-head.php
@@ -4,5 +4,22 @@
         <img src="<?php echo get_template_directory_uri() . '/img/shark_logo-header.png' ?>"/>
     </a>
 </div>
+<script type="text/javascript">
+    var _gaq = _gaq || [];
+    var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+    _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+    _gaq.push(['_setAccount', 'UA-19899321-1']);
+    _gaq.push(['_setDomainName', '.tradablebits.com']);
+    _gaq.push(['_trackPageview']);
+
+    (function () {
+        var ga = document.createElement('script');
+        ga.type = 'text/javascript';
+        ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga, s);
+    })();
+</script>
 <div class="sharks-container">
 
